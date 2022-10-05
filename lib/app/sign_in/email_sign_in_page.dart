c3c0c50ffbe8d0ddea415536a1c35d6e3b52d418
Trine Staverløkk <trinemst@ntnu.no>
2022-10-05 10:52:53 +0200

    created EmailSignInPage, and made it a stateless widget. Made the build-methid and created a buildContent-widget. Not imp

---
+++ lib/app/sign_in/email_sign_in_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EmailSignInPage extends StatelessWidget {
+  //const EmailSignInPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Container();
+  }
+}
