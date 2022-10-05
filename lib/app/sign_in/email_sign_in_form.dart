65023a891242bbba287d2b383bbb26632f2f7439
Trine Staverløkk <trinemst@ntnu.no>
2022-10-05 13:28:00 +0200

    Toggling the form type

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker_flutter_course/common_widgets/form_submit_button.dart';
 
+enum EmailSignInFormType { signIn, register }
+
 class EmailSignInForm extends StatefulWidget {
   const EmailSignInForm({Key? key}) : super(key: key);
 
@@ -9,24 +11,36 @@ class EmailSignInForm extends StatefulWidget {
 }
 
 class _EmailSignInFormState extends State<EmailSignInForm> {
-
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
 
   void _submit() {
     print('email: ${_emailController.text}, password: ${_passwordController}');
   }
 
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+    });
+    _emailController.clear();
+    _passwordController.clear();
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText =
+        _formType == EmailSignInFormType.signIn ? 'Sign in' : 'Create account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an Account? Sign in';
     return [
       TextField(
         controller: _emailController,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          hintText: 'test@email.com'
-        ),
-
+        decoration:
+            InputDecoration(labelText: 'Email', hintText: 'test@email.com'),
       ),
       SizedBox(height: 8.0),
       TextField(
@@ -38,13 +52,13 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        text: 'Sign in',
+        text: primaryText,
         onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       TextButton(
-          child: Text('Need an account? Register'),
-        onPressed: () {},
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
       ),
     ];
   }
---
+++ lib/services/enums/email_sign_in_form_type.dart
@@ -0,0 +1,7 @@
+
+
+/*
+enum EmailSignInFormType {
+  signIn,
+  register
+}*/
