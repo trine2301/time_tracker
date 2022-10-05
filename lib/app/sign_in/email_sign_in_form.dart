ed151ddf8ebd806ff292ed7cf9a822859db63184
Trine Staverløkk <trinemst@ntnu.no>
2022-10-05 11:30:41 +0200

    added the Email and Password-text fields in EmailSignInForm. made the password obscure text and they are both inputs.

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -3,8 +3,31 @@ import 'package:flutter/material.dart';
 class EmailSignInForm extends StatelessWidget {
   const EmailSignInForm({Key? key}) : super(key: key);
 
+  List<Widget> _buildChildren() {
+    return [
+      const TextField(
+        decoration: InputDecoration(
+          labelText: 'Email',
+          hintText: 'test@email.com'
+        ),
+      ),
+      const TextField(
+        decoration: InputDecoration(
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      ),
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: _buildChildren(),
+      ),
+    );
   }
 }
